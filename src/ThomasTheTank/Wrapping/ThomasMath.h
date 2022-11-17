7d0b53ed6d01757f057934b27f1276e4f9b39684
harry2512tea <benjamin.t.harrison24@gmail.com>
2022-11-17 13:40:19 +0000

    imported physics engine library

--- src/ThomasTheTank/Wrapping/ThomasMath.h
+++ src/ThomasTheTank/Wrapping/ThomasMath.h
@@ -2,10 +2,12 @@
 #define THOMASTHETANK_THOMASMATH_H
 
 #include <rend/rend.h>
+#include <reactphysics3d/reactphysics3d.h>
 
 namespace ThomasTheTank
 {
 	using namespace glm;
+	using namespace reactphysics3d;
 }
 
 #endif
